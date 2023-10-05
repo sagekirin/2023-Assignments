568cbaef81874a3f6451164d45517d6a0038134d
Sage K <62972988+sagekirin@users.noreply.github.com>
2023-10-05 11:04:19 -0400

    Add files via upload

---
+++ Assignment3B.java
@@ -0,0 +1,88 @@
+/*
+Class: CSE 1321L
+Section: WJ3
+Term: Fall 2023
+Instructor: Harshitha Nirujogi
+Name: Alani Harrell
+Assignment#: 3
+*/
+
+import java.util.Scanner;
+
+public class Assignment3B {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String choice;
+        int total = 0;
+
+        System.out.println("[World Traveler Checklist]");
+
+        System.out.println("Have you been to Africa?");
+        choice = sc.nextLine();
+        if (choice.equals("Y")) {
+            total++;
+        }
+
+        System.out.println("Have you been to Antarctica?");
+        choice = sc.nextLine();
+        if (choice.equals("Y")) {
+            total++;
+        }
+
+        System.out.println("Have you been to Asia?");
+        choice = sc.nextLine();
+        if (choice.equals("Y")) {
+            total++;
+        }
+
+        System.out.println("Have you been to Australia?");
+        choice = sc.nextLine();
+        if (choice.equals("Y")) {
+            total++;
+        }
+
+        System.out.println("Have you been to Europe?");
+        choice = sc.nextLine();
+        if (choice.equals("Y")) {
+            total++;
+        }
+
+        System.out.println("Have you been to North America?");
+        choice = sc.nextLine();
+        if (choice.equals("Y")) {
+            total++;
+        }
+
+        System.out.println("Have you been to South America?");
+        choice = sc.nextLine();
+        if (choice.equals("Y")) {
+            total++;
+        }
+
+        switch (total) {
+            case 0:
+                System.out.println("You have visited " + total + " continents: How is that possible? Are you living in space?");
+                break;
+            case 1:
+                System.out.println("You have visited " + total + " continents: That’s a good start. Time to explore the world!");
+                break;
+            case 2:
+            case 3:
+                System.out.println("You have visited " + total + " continents: You’ve just started your journey to see the world.");
+                break;
+            case 4:
+            case 5:
+                System.out.println("You have visited " + total + " continents: You’re an experienced traveler now.");
+                break;
+            case 6:
+                System.out.println("You have visited " + total + " continents: Wow, you’ve been almost everywhere!");
+                break;
+            case 7:
+                System.out.println("You have visited " + total + " continents: You’re a true world traveler!");
+                break;
+        }
+    }
+}
