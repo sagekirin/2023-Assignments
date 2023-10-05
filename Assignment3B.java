de06741d162299f18373f9f003878401a7861ecd
Sage K <62972988+sagekirin@users.noreply.github.com>
2023-10-05 11:07:07 -0400

    Update Assignment3B.java

--- Assignment3B.java
+++ Assignment3B.java
@@ -1,30 +1,21 @@
-/*
-Class: CSE 1321L
-Section: WJ3
-Term: Fall 2023
-Instructor: Harshitha Nirujogi
-Name: Alani Harrell
-Assignment#: 3
-*/
-
 import java.util.Scanner;
 
 public class Assignment3B {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);  //Starting scanner named sc
 
         String choice;
-        int total = 0;
+        int total = 0;  //Assigning and initalizing values
 
         System.out.println("[World Traveler Checklist]");
 
         System.out.println("Have you been to Africa?");
         choice = sc.nextLine();
         if (choice.equals("Y")) {
-            total++;
-        }
+            total++; //adding a count to the total amount of countries if condition is met
+        }  //Else statement isn't required
 
         System.out.println("Have you been to Antarctica?");
         choice = sc.nextLine();
@@ -62,14 +53,14 @@ public class Assignment3B {
             total++;
         }
 
-        switch (total) {
+        switch (total) {  //Using the total number tallied from earlier to comment accordingly
             case 0:
                 System.out.println("You have visited " + total + " continents: How is that possible? Are you living in space?");
                 break;
             case 1:
                 System.out.println("You have visited " + total + " continents: That’s a good start. Time to explore the world!");
                 break;
-            case 2:
+            case 2: //Because the range is 2 - 3, a blank case can be stacked on top of a written one
             case 3:
                 System.out.println("You have visited " + total + " continents: You’ve just started your journey to see the world.");
                 break;
